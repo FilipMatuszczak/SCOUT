6a390f39a2d796f7c67591445f4f6340ebafd6d3
filip.matuszczak <chips1331@gmail.com>
2019-05-14 12:23:07 +0200

    Fixes for login

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -12,6 +12,7 @@ class MainController extends AbstractController
 
     public function mainAction()
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         return $this->render('main/main.html.twig', []);
     }
 }

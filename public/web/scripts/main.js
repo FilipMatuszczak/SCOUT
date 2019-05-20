cb574827d4f2160510c66f3c2a4924119a7ff126
Ewapotranspiracja <32907073+Ewapotranspiracja@users.noreply.github.com>
2019-05-20 17:32:38 +0200

    Checking username actually blocks signing form
    and its works when user stops typing and its PRETTY

--- public/web/scripts/main.js
+++ public/web/scripts/main.js
@@ -1,27 +1,31 @@
 $(document).ready(function(){
-  
     
+    var username_free = false;
     
+    //////////////////function for end typing
+    (function($) {
+$.fn.donetyping = function(callback){
+    var _this = $(this);
+    var x_timer;    
+    _this.keyup(function (){
+        clearTimeout(x_timer);
+        x_timer = setTimeout(clear_timer, 1000);
+    }); 
+
+    function clear_timer(){
+        clearTimeout(x_timer);
+        callback.call(_this);
+    }
+}
+})(jQuery);
     
-    $('#username').keyup(function()
-                       
+    //////Function for checking if username is available
+  $('#username').donetyping(function(callback)           
                        {
-        var username = $('#username').val()
+  $('#username').css('border','none');
+  var username = $('#username').val()
        
-        
     
-        /////////
-       
- 
-       
-        
-       
-        
-        
-        
-        
-    
-    //////////////
     if(username!=''){
         
          
@@ -32,22 +36,21 @@ $(document).ready(function(){
         
 request.open('GET', 'http://127.0.0.1:8000/user_exists/'+username, true)
 request.onload = function() {
-  // Begin accessing JSON data here
+    
   var data = JSON.parse(this.response)
 
   if (request.status >= 200 && request.status < 400) {
   
-       // console.log(username);   
-        //console.log(data.user_exists);    
-                
-      
       if (data.user_exists == true){
-         // console.log("ISTNIEJE");
-              $('#status').html('<p>ISTNIEJE</p>');
+            $('#status').css('color','darkred');
+              $('#status').html('<b>Login jest już zajęty!</b>');
+         username_free = false;
       }
       else {
-         // console.log("WOLNE");
-              $('#status').html('<p>WOLNE</p>');
+          
+            $('#status').css('color','black');
+              $('#status').html('<b>Login dostępny!</b>');
+          username_free = true;
       }
       
   } else {
@@ -60,16 +63,29 @@ request.send()
         
     }
     else {
-        $('#status').html('');
+         $('#status').html('<b>Nazwa użytkownika</b>');
+    $('#status').css('color','black');
     }
     
     });
 
 
   
+    ////////function for submiting form
     
+    $('#register-form').click(function() {
+        if(username_free){ //if form is valid submit form
+			return true;
+		}
+         $('#username').css('border','1.5px solid red');
+		event.preventDefault();
+        
+    }
+           
+        
+        
     
-    
+                              );
     
     
     

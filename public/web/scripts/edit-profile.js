98257a77997e338374bba28ebdab7c6b9b333b5a
Ewapotranspiracja <32907073+Ewapotranspiracja@users.noreply.github.com>
2019-05-28 18:41:51 +0200

    new features in edit profile
    also profile fixed

---
+++ public/web/scripts/edit-profile.js
@@ -0,0 +1,185 @@
+$(document).ready(function(){
+    
+    //////Function for getting technologies
+  $('#tech').keyup(function(callback)           
+                       {
+  
+  var tech = $('#tech').val()
+       
+    ///console.log(lang);
+    if(tech!=''){
+        
+         
+        
+        
+        var request = new XMLHttpRequest()
+        var exists = 0;
+        
+request.open('GET', 'http://127.0.0.1:8000/filter/technology/'+ tech, true)
+request.onload = function() {
+    
+  var data = JSON.parse(this.response)
+$('#TechList').html('');
+  
+  if (request.status >= 200 && request.status < 400) {
+    
+      for (i = 0; i<data.names.length;i++){
+      ////console.log(data.names[i].name);
+          
+          if(data.names[i].name != tech){
+          
+      $('#TechList').append('<option value="' + data.names[i].name + '">');
+      }
+  }
+  } else {
+    console.log('error')
+  }
+  
+  
+  
+}
+
+request.send()
+        
+        
+    }
+    else {
+         
+    }
+    
+    });
+//////
+
+  
+    
+    ////getting languages
+    $('#lang').keyup(function(callback)           
+                       {
+  
+  var lang = $('#lang').val()
+       
+    ///console.log(lang);
+    if(lang!=''){
+        
+         
+        
+        
+        var request = new XMLHttpRequest()
+        var exists = 0;
+        
+request.open('GET', 'http://127.0.0.1:8000/filter/language/'+ lang, true)
+request.onload = function() {
+    
+  var data = JSON.parse(this.response)
+$('#LangList').html('');
+  
+  if (request.status >= 200 && request.status < 400) {
+    
+      for (i = 0; i<data.names.length;i++){
+      ////console.log(data.names[i].name);
+          
+          if(data.names[i].name != lang){
+          
+      $('#LangList').append('<option value="' + data.names[i].name + '">');
+      }
+  }
+  } else {
+    console.log('error')
+  }
+  
+  
+  
+}
+
+request.send()
+        
+        
+    }
+    else {
+         
+    }
+    
+    });
+  /////////////////////////////
+    
+    
+    
+    
+     ////getting cities
+    $('#city').keyup(function(callback)           
+                       {
+  
+  var city = $('#city').val()
+       
+    ///console.log(lang);
+    if(city!=''){
+        
+         
+        
+        
+        var request = new XMLHttpRequest()
+        var exists = 0;
+        
+request.open('GET', 'http://127.0.0.1:8000/filter/city/'+ city, true)
+request.onload = function() {
+    
+  var data = JSON.parse(this.response)
+$('#CityList').html('');
+  
+  if (request.status >= 200 && request.status < 400) {
+    
+      for (i = 0; i<data.names.length;i++){
+      ////console.log(data.names[i].name);
+          
+          if(data.names[i].name != city){
+          
+      $('#CityList').append('<option value="' + data.names[i].name + '">');
+      }
+  }
+  } else {
+    console.log('error')
+  }
+  
+  
+  
+}
+
+request.send()
+        
+        
+    }
+    else {
+         
+    }
+    
+    });
+  /////////////////////////////
+    
+    
+    
+    
+    
+    
+    //////// TODO  function for submiting form
+    
+    $('#register-form').click(function() {
+        if(username_free){ //if form is valid submit form
+			return true;
+		}
+         $('#username').css('border','1.5px solid red');
+		event.preventDefault();
+        
+    }
+           
+        
+        
+    
+                              );
+    
+    
+    
+});
+
+
+
+

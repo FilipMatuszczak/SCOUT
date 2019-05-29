f0590fd801ae7fe9206dc06e29ace91ad37ecca6
Ewapotranspiracja <32907073+Ewapotranspiracja@users.noreply.github.com>
2019-05-29 19:55:48 +0200

    no more commas

--- public/web/scripts/edit-profile.js
+++ public/web/scripts/edit-profile.js
@@ -158,6 +158,27 @@ request.send()
     
     
     
+    //////////////photo preview
+    function readURL(input) {
+
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      $('#photo-edit').attr('src', e.target.result);
+    }
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+$("#fileToUpload").change(function() {
+  readURL(this);
+});
+   //// 
+    
+    
+    
     
     
     //////// TODO  function for submiting form
